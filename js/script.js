dfa08f7f1a73539c53b511f4ac638bf0a49f04a5
dtsnell4 <dtsnell4@yahoo.com>
2016-04-26 14:46:04 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function dropdown() {
+   var menu = document.getElementById("nav");
+   if(menu.style.display == 'block')
+      menu.style.display = 'none';
+   else
+      menu.style.display = 'block';
+}
